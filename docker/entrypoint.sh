cae3f650ee9f6f8d55916f1e259ce691dd666d7e
alimiSM <71573842@usat.pe>
2024-06-22 13:05:12 -0500

    docker-file-improve-with-key-auto-envfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 set -e
 
-# Run composer install
+# Aumentar el tiempo de espera de Composer a 1200 segundos (20 minutos)
+export COMPOSER_PROCESS_TIMEOUT=1200
+
+# Copiar .env.example a .env si .env no existe
+if [ ! -f /var/www/.env ]; then
+    cp /var/www/.env.example /var/www/.env
+fi
+
+# Generar APP_KEY si no está configurada
+if ! grep -q "APP_KEY=base64:" /var/www/.env; then
+    php artisan key:generate
+fi
+
+# Ejecutar composer install
 composer install --no-dev --optimize-autoloader
 
-# Run migrations and seeders
+# Ejecutar migraciones y seeders
 php artisan migrate --seed
 
-# Then start PHP-FPM
+# Iniciar PHP-FPM
 php-fpm
