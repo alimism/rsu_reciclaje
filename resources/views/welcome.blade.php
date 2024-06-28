520914f5357b6e6c3c7bccea67e7278f7f7ed5e0
Alison S.M. <71573842@usat.pe>
2024-06-28 18:35:24 -0500

    welcome-2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -135,6 +135,10 @@
             background-image: none
         }
 
+        logoWelcome {
+            width: 100px
+        }
+
         :-moz-focusring {
             outline: auto
         }
@@ -826,7 +830,7 @@
     </style>
 </head>
 
-<body class="antialiased">
+{{-- <body class="antialiased">
     <div
         class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 selection:bg-red-500 selection:text-white">
         @if (Route::has('login'))
@@ -980,6 +984,88 @@
                             </p>
                         </div>
                     </div>
+                </div>
+            </div>
+
+            <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
+                <div class="text-center text-sm text-gray-500 sm:text-left">
+                    <div class="flex items-center gap-4">
+                        <a href="https://github.com/sponsors/taylorotwell"
+                            class="group inline-flex items-center hover:text-gray-700 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                class="-mt-px mr-1 w-5 h-5 stroke-gray-400 group-hover:stroke-gray-600">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                            </svg>
+                            Sponsor
+                        </a>
+                    </div>
+                </div>
+
+                <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                    Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                </div>
+            </div>
+        </div>
+    </div>
+</body> --}}
+
+<body class="antialiased">
+    <div
+        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 selection:bg-red-500 selection:text-white">
+        @if (Route::has('login'))
+            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+                @auth
+                    <a href="{{ url('/dashboard') }}"
+                        class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Inicio</a>
+                @else
+                    <a href="{{ route('login') }}"
+                        class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Iniciar
+                        sesión</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}"
+                            class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Registrarse</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+
+        <div class="max-w-7xl mx-auto p-6 lg:p-8">
+            <div class="flex justify-center">
+                <img id="logoWelcome" src="{{ asset('favicons/favicon.png') }}" alt="Gestión de Residuos Logo"
+                    class="w-100">
+            </div>
+
+            <div class="mt-6 text-center">
+                <h1 class="text-xl font-semibold text-gray-900 text-center">Gestión de Residuos</h1>
+                <p class="mt-6 text-gray-700 text-lg">Facilitamos la gestión de residuos a través de nuestras
+                    plataformas web y móvil.</p>
+            </div>
+
+            <div class="mt-6 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+                <div class="p-6 bg-white rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300">
+                    {{-- <div class="h-16 w-16 bg-green-50 flex items-center justify-center rounded-full mx-auto">
+                        <i class="fa-solid fa-desktop"></i>
+                    </div> --}}
+                    <h2 class="mt-6 text-lg font-semibold text-gray-900 text-center">Plataforma Web</h2>
+                    <p class="mt-4 text-gray-600 text-sm leading-relaxed text-center">
+                        Administrada por la Municipalidad XYZ quienes se encargan de la gestión del proceso de recojo de
+                        residuos manejando las unidades, rutas, zonas y programación.
+                    </p>
+                </div>
+
+                <div class="p-6 bg-white rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300">
+                    {{-- <div class="h-16 w-16 bg-blue-50 flex items-center justify-center rounded-full mx-auto">
+                        <i class="fas fa-mobile-alt text-blue-500 text-3xl"></i>
+                    </div> --}}
+                    <h2 class="mt-6 text-lg font-semibold text-gray-900 text-center">Aplicación Móvil</h2>
+                    <p class="mt-4 text-gray-600 text-sm leading-relaxed text-center">
+                        Habilitada para los ciudadanos, recolectores y choferes encargadas con la finalidad de poder dar
+                        a conocer cuando pasa el camión por la zona seleccionada por un ciudadano a traves del recorrido
+                        de rutas realizada por los choferes y recolectores por diversas zonas.
+                    </p>
                 </div>
             </div>
 
